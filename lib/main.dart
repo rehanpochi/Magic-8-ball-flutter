6f04a0bb23a2db8f372d66907201fb0a14f2c0bc
Rehan Pochi <rehanpochi1@gmail.com>
2021-09-18 17:31:28 +0530

    Initial commit
    Signed-off-by: Rehan Pochi <rehanpochi1@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,57 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          backgroundColor: Colors.grey,
+          title: Center(
+            child: Text(
+              'Ask Any Questions',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ),
+        body: ball(),
+      ),
+    ),
+  );
+}
+
+class ball extends StatefulWidget {
+  @override
+  _ballState createState() => _ballState();
+}
+
+class _ballState extends State<ball> {
+  @override
+  int number = 4;
+
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  number = Random().nextInt(5) + 1;
+                });
+                print("pressed");
+              },
+              child: Image.asset('images/ball$number.png'),
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
